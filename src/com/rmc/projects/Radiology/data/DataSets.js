b4627fd9070f2c15f476ca86f382960ca6e7d330
neeti1992 <neeti1992@gmail.com>
2013-08-31 10:51:09 +0530

    Create DataSets.js

---
+++ src/com/rmc/projects/Radiology/data/DataSets.js
@@ -0,0 +1,20 @@
+Ext.define('com.rmc.projects.Radiology.data.DataSets', {
+    //singleton: true,
+    
+    OrderType: [
+       ['abc'],
+        ['pqr'],
+        ['xyz']
+    ],
+     Modality: [
+       ['X-Ray'],
+        ['MRI'],
+        ['Ultrasound']
+    ],
+     Priority: [
+       ['High'],
+        ['Medium'],
+        ['Low']
+    ],
+
+});
