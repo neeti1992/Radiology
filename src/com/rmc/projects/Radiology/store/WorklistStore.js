b92e8994add94444cebb3411a86cbff863ed935d
neeti1992 <neeti1992@gmail.com>
2013-09-20 12:07:16 +0530

    Update WorklistStore.js

--- src/com/rmc/projects/Radiology/store/WorklistStore.js
+++ src/com/rmc/projects/Radiology/store/WorklistStore.js
@@ -2,22 +2,22 @@ Ext.define('com.rmc.projects.Radiology.store.WorklistStore', {
     extend: 'Ext.data.Store',
     model: 'com.rmc.projects.Radiology.model.WorklistModel',
     storeId: 'WorklistStore',
-    // data: [
-    //     ['xyz', 'Low', 'X-Ray', 'Right ankle', '1/1/2014', '4/4/2014', 'Uncomplete'],
-    //     ['abc', 'High', 'MRI', 'Ribs', '1/1/2014', '4/4/2014', 'Complete']
-    // ]
     autoLoad: true,
     // autoSync: false,
     proxy: {
-        type: 'ajax',
-       /* url:                                          //after backend is done
+       /* 
+        type: 'rest'
+        url:                                          //after backend is done
         headers: MainController.getBasicAuthHeaders(),
         reader: {
             type:'json',
+            //check if record has to be mentioned
             root: 'results'
         }*/
+                     
+
                      // temporoary solution
-                    // load remote data using HTTP*/
+                    type: 'ajax',
                     url: 'resources/data/worklist.xml',
                     // specify a XmlReader (coincides with the XML format of the returned data)
                     reader: {
