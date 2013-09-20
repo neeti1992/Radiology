507eaf1c239279e6a0c5fb370454f0bd573fcafa
neeti1992 <neeti1992@gmail.com>
2013-09-20 12:02:30 +0530

    Update Modality.js

--- src/com/rmc/projects/Radiology/store/Modality.js
+++ src/com/rmc/projects/Radiology/store/Modality.js
@@ -1,18 +1,6 @@
 /*
  * Copyright 2013, Raxa
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- * 
  * This file lists modalities
  */
 Ext.define('com.rmc.projects.Radiology.store.Modality', {
@@ -24,8 +12,10 @@ Ext.define('com.rmc.projects.Radiology.store.Modality', {
     model : 'com.rmc.projects.Radiology.model.Modality',
     storeId: 'Modality',
     data: [
-        ['X-Ray'],
-        ['MRI'],
-        ['Ultrasound']
+        ['CR'],
+        ['MR'],
+        ['CT'],
+        ['NM'],
+        ['US']
     ]
 });
